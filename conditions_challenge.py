d2166e717e70c49849b8b11f102dfe7857b06e51
123WorkID <michael.bray@aglc.ca>
2025-10-30 17:23:03 +0000

    while loops PYTHON

--- conditions_challenge.py
+++ conditions_challenge.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python3
-"""Jurassic Park Trivia | Solution"""
+"""Jurassic Park Trivia | Solution with While True Loops"""
 
 # Question 1
-question1 = input("In Jurassic Park, what kind of dinosaur is the star attraction? ")
-
-# Check if the answer is correct (case-insensitive)
-if question1.lower() == "t-rex":
-    print("Correct! The T-Rex is the star attraction!")
-else:
-    print("Sorry, that's not correct. The correct answer is T-Rex.")
+attempts = 0
+while True:
+    question1 = input("In Jurassic Park, what kind of dinosaur is the star attraction? ")
+    if question1.lower() == "t-rex":
+        print("Correct! The T-Rex is the star attraction!")
+        break
+    else:
+        print("Sorry, that's not correct.")
+        attempts += 1
+    if attempts == 3:
+        print("The correct answer is T-Rex.")
+        break
 
 # Question 2
-question2 = input("What type of DNA was used to fill the gaps in the dinosaur genome? ")
-
-# Check if the answer is correct (case-insensitive)
-if question2.lower() == "frog":
-    print("Correct! Frog DNA was used to fill the gaps in the genome!")
-else:
-    print("Sorry, that's not correct. The correct answer is Frog.")
+attempts = 0
+while True:
+    question2 = input("What type of DNA was used to fill the gaps in the dinosaur genome? ")
+    if question2.lower() == "frog":
+        print("Correct! Frog DNA was used to fill the gaps in the genome!")
+        break
+    else:
+        print("Sorry, that's not correct.")
+        attempts += 1
+    if attempts == 3:
+        print("The correct answer is Frog.")
+        break
 
